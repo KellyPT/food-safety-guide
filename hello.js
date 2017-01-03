4f9fb454ae0eb3d5d194e434648ad1ad8e2af95c
KellyPT <trankphuong@gmail.com>
2017-01-03 14:47:40 -0800

    Create a server and 2 event listeners for an emitted request

--- hello.js
+++ hello.js
@@ -1,9 +1,33 @@
+// non-blocking events
+
+// var http = require('http');
+// var fs = require('fs');
+//potentially can be used to call my API file.
+
+// http.createServer(function(request, response) {
+//   response.writeHead(200);
+//   response.write("hello world!");
+//   response.end();
+  // fs.readFile("index.html", function(error,contents){
+  //   response.write(contents);
+  //   response.end();
+  // });
+
+// }).listen(8080);
+// listen with $curl http://localhost:8080
+
+// Create a Server and event
 var http = require('http');
-var fs = require('fs');
+var server = http.createServer();
 
-http.createServer(function(request, response) {
+server.on('request', function(request, response) {
   response.writeHead(200);
-  response.write("hello world!");
+  response.write("Hello, this is dog");
   response.end();
+});
 
-}).listen(8080);
+server.on('request', function(request,response){
+  console.log("Hello from the other side");
+});
+
+server.listen(8080);
