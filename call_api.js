767ce2925da101a44f05ec44da882c8b167dc955
KellyPT <trankphuong@gmail.com>
2017-01-04 00:04:30 -0800

    Calling API from data.kingcounty.gov with Express. Success.

---
+++ call_api.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var url = require('url');
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+  options = {
+    protocol: "https:",
+    host: "data.kingcounty.gov",
+    pathname: "/resource/gkhn-e8mn.json",
+    data : {"$limit" : 5}
+  };
+
+  var dataURL = url.format(options);
+  request(dataURL).pipe(res);
+}).listen(8080);
