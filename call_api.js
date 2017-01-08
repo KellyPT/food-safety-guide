df22aef043dfae1c3f18c4e95a8b6c1de33da006
KellyPT <trankphuong@gmail.com>
2017-01-08 13:29:04 -0800

    changed node url format to limit data to Seattle region

--- call_api.js
+++ call_api.js
@@ -9,37 +9,18 @@ app.get('/', function(req, response){
     protocol: "https:",
     host: "data.kingcounty.gov",
     pathname: "/resource/gkhn-e8mn.json",
-    data : {"$limit" : 1}
+    search: "city=Seattle&$limit=100"
   };
 
   var dataURL = url.format(options);
+  console.log(dataURL);
   // request(dataURL).pipe(res);
 
   request(dataURL, function(err, res, body){
-    var apiName = JSON.parse(body)[0].inspection_business_name;
-    var apiID = JSON.parse(body)[0].business_id;
-    var apiAddress = JSON.parse(body)[0].address;
-    var apiCity = JSON.parse(body)[0].city;
-    var apiZipCode = JSON.parse(body)[0].zip_code;
-    var apiLongitude = JSON.parse(body)[0].longitude;
-    var apiLatitude = JSON.parse(body)[0].latitude;
-    var apiInspectionDate = JSON.parse(body)[0].inspection_date;
-    var apiInspectionResult = JSON.parse(body)[0].inspection_result;
-    var apiViolationType = JSON.parse(body)[0].violation_type;
-    var apiViolationDescription = JSON.parse(body)[0].violation_description;
-    response.locals = {
-      apiName: apiName,
-      apiID: apiID,
-      apiAddress: apiAddress,
-      apiCity: apiCity,
-      apiZipCode: apiZipCode,
-      apiLongitude: apiLongitude,
-      apiLatitude: apiLatitude,
-      apiInspectionDate: apiInspectionDate,
-      apiInspectionResult: apiInspectionResult,
-      apiViolationType: apiViolationType,
-      apiViolationDescription: apiViolationDescription
-    };
+    var results = JSON.parse(body);
+    response.locals = {results: results};
     response.render('test.ejs');
   });
-}).listen(8080);
+});
+
+app.listen(8080);
